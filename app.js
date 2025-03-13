13af28ad15679e244b482abaa4650ece84f90336
ysaa <ysagabrielle.cervantes@gmail.com>
2025-03-13 22:09:54 +0800

    Added blog feature with dynamic data fetching

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var port = 5000; 
+var port = 5000;
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +24,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Use existing routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Blog route to read posts.json
+test 
+app.get('/blog', (req, res) => {
+  fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to load posts' });
+    }
+    res.json(JSON.parse(data));
+  });
+});
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
